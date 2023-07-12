afa349578188211b2c81644d2fe7e0880ead4546
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2023-07-12 20:30:51 +0200

    responsive changes in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,14 +11,14 @@ const Cart = () => {
             {products?.length > 0 ? products?.map((product, i) =>
 
                 <div key={i} className='p-4 mx-2 my-1 rounded-lg flex transition hover:bg-gray-800'>
-                    <div className='w-1/5'>
+                    <div className='lg:w-1/5 w-2/5'>
                         <div className='p-2 rounded-lg bg-white w-fit'>
                             <img className='max-h-16' src={product.imgUrl} alt={product.model} />
                         </div>
                     </div>
-                    <div className='w-4/5 flex flex-col'>
-                        <span className='text-xl font-semibold border-b border-white'>{product.brand} | {product.model}</span>
-                        <span className='text-3xl text-white font-semibold'>{product.price ? product.price + '€' : ''} </span>
+                    <div className='lg:w-4/5 w-3/5 flex flex-col'>
+                        <span className='lg:text-xl text-lg font-semibold border-b border-white'>{product.brand} | {product.model}</span>
+                        <span className='lg:text-3xl text-2xl text-white font-semibold'>{product.price ? product.price + '€' : ''} </span>
                     </div>
                 </div>
             ) : <div>No hay productos</div>}
